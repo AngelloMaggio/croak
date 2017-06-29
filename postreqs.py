1224e9b74ad2dc28963550bba1e4a1b6dd132e76
Angello Maggio <angellom@jfrog.com>
2017-06-29 13:47:29 -0700

    fixed bug that made croak crash whe nno request type was given

--- postreqs.py
+++ postreqs.py
@@ -1,8 +1,8 @@
 post_requests = {'make_bundle': 'api/support/bundles',
-            'optimize_storage': 'api/system/storage/optimize',
-            'build_promote': 'api/build/promote/INC',
-            'docker_promote': 'api/docker/INC',
-            'build_rename': 'api/build/rename/INC'}
+                 'optimize_storage': 'api/system/storage/optimize',
+                 'build_promote': 'api/build/promote/INC',
+                 'docker_promote': 'api/docker/INC',
+                 'build_rename': 'api/build/rename/INC'}
 
 post_functions = {'build_promote': lambda x, y: build_promote(x, y),
                   'docker_promote': lambda x, y: docker_promote(x, y),
@@ -12,16 +12,13 @@ post_functions = {'build_promote': lambda x, y: build_promote(x, y),
 def build_rename(data, params):
 
     args = params.split(' ')
-
     new_url = post_requests[data[0]][:-3] + '/' + args[0] + '/' + args[1]
-
     return new_url
 
 
 def docker_promote(data, params):
 
     new_url = post_requests[data[0]][:-3] + '/' + params + '/v2/promote'
-
     return new_url
 
 
@@ -29,5 +26,4 @@ def build_promote(data, params):
 
     args = params.split(' ')
     new_url = post_requests[data[0]][:-3] + '/' + args[0] + '/' + args[1]
-
     return new_url
